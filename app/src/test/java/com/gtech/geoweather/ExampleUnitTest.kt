7d2a6c65b5e53793752da119d260f9f1a9bfd059
giocornejo <gio.cornejo@stationfive.com>
2023-03-01 10:22:49 +0800

    Setup Unit Testing

--- app/src/test/java/com/gtech/geoweather/ExampleUnitTest.kt
+++ app/src/test/java/com/gtech/geoweather/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package com.gtech.geoweather
 
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.launchActivity
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +17,14 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+
+    private lateinit var scene : ActivityScenario<WelcomeActivity>
+
+    @Before
+    fun initiate(){
+        scene = launchActivity()
+        scene.moveToState(Lifecycle.State.STARTED)
     }
 }
