aac621a56f1f91f4f10f79d8752c3cdef72d0ad2
giocornejo <gio.cornejo@stationfive.com>
2023-03-01 22:41:59 +0800

    Setup Landing Page with 2 fragments as a requirement and a bottom tab. The 2 fragments are for the current weather and the history.

---
+++ app/src/main/java/com/gtech/geoweather/sections/fragment_wather_history/WeatherHistoryViewModel.kt
@@ -0,0 +1,7 @@
+package com.gtech.geoweather.sections.fragment_wather_history
+
+import androidx.lifecycle.ViewModel
+
+class WeatherHistoryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/gtech/geoweather/sections/fragment_weather_home/HomeWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.gtech.geoweather.sections.fragment_weather_home
+
+import androidx.lifecycle.ViewModel
+
+class HomeWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
